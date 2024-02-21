14fbab7aa12580215b99f468ba6b7bf389dce326
RexJustesen <rexjustesen1992@gmail.com>
2024-02-20 19:42:18 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+import { Col, Row,Container} from 'react-bootstrap';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import WaitingRoom from './components/waitingroom';
+import { useState } from 'react';
+import { HubConnectionBuilder, LogLevel } from '@microsoft/signalr';
+import ChatRoom from './components/ChatRoom';
 
 function App() {
+  const [conn, setConnection] = useState();
+  const [messages, setMessages] = useState([]);
+
+  const joinChatRoom = async (username, chatroom) => {
+    try {
+      //initiate a connection
+      const conn = new HubConnectionBuilder()
+        .withUrl("http://localhost:5124/chat")
+        .configureLogging(LogLevel.Information)
+        .build();
+    // set up handler 
+      conn.on("JoinSpecificChatRoom", (username, msg) => {
+        console.log("msg ", msg);
+        
+      })
+
+      conn.on("ReceiveSpecificMessage", (username, msg) => {
+        console.log("msg", msg);
+        setMessages(messages => [...messages, { username, msg }]);
+    });
+
+      await conn.start();
+      await conn.invoke("JoinSpecificChatRoom", {username, chatroom});
+
+      setConnection(conn);
+
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
+  const sendMessage = async(message) => {
+    console.log("msg ", message);
+    try{
+      await conn.invoke("SendMessage", message);
+    }catch (e){
+      console.log(e);
+    }
+  }
+
+  console.log(messages);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <main>
+        <Container>
+          <Row class='px-5 my-5'>
+            <Col sm='12'>
+              <h1 className='font-weight-light'>Welcome to the ChatApp</h1>
+            </Col>
+          </Row>
+          { !conn 
+            ? <WaitingRoom joinChatRoom={joinChatRoom}></WaitingRoom>
+            : <ChatRoom messages={messages} sendMessage={sendMessage}></ChatRoom>
+          }
+        </Container>
+      </main>
     </div>
   );
 }
---
+++ src/components/SendMessageForm.js
@@ -0,0 +1,20 @@
+import { useState } from "react"
+import { Form, InputGroup, Button } from "react-bootstrap";
+
+const SendMessageForm = ({ sendMessage }) => {
+    const[msg, setMessage] = useState('');
+
+    return <Form onSubmit={e =>{
+        e.preventDefault();
+        sendMessage(msg);
+        setMessage('');
+    }}>
+        <InputGroup className="mb-3">
+            <InputGroup.Text>Chat</InputGroup.Text>
+            <Form.Control onChange={e => setMessage(e.target.value)} value={msg} placeholder="Type a message"></Form.Control>
+            <Button variant="primary" type="submit" disabled={!msg}>Send</Button>
+        </InputGroup>
+    </Form>
+}
+
+export default SendMessageForm;
